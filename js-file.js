3d30f3c4b1138a561d8a4db314e8b37ae8e9cd52
Stephzennn <estifanosSNzen@outlook.com>
2024-03-27 13:29:25 +0300

    Construct the display

--- js-file.js
+++ js-file.js
@@ -47,6 +47,8 @@ let buttonBox = document.querySelector(".buttonBox")
 //buttonBox.appendChild(newButtons)
 
 let allOperationString = ["÷","CE",")","(","√x","×",9,8,7,"xʸ","-",6,5,4,"10ʸ","+",3,2,1,"log", "=",".",0,"+/-","ln"]
+let allClasses = ["÷","CE","rightBraket","leftBracket","Radical","×","nine","eight","seven","power","-","six",
+"five","four","tenPower","+","three","two","one","log", "=","point","zero","changeSign","ln"]
 
    
 
@@ -59,20 +61,47 @@ for (let ver = 0 ; ver < 5; ver++){
         let newButtons = document.createElement('button')
         newButtons.classList.add("numberButton")
         newButtons.innerText = allOperationString[counter]
-        newButtons.id = allOperationString[counter]
+        if (["+","-","÷","×", "="].includes(allOperationString[counter])){
+            switch (allOperationString[counter]){
+                case "+":
+                    newButtons.classList.add("plus")
+                    break
+                case "-":
+                    newButtons.classList.add("minus")
+                    break
+                case "÷":
+                    newButtons.classList.add("of")
+                    break
+                case "×":
+                    newButtons.classList.add("multiply")
+                    break
+                case "=":
+                    newButtons.classList.add("equals")
+                    break
+                default:
+                    break
+
+            }
+            
+        }
+        else {
+            newButtons.classList.add(allClasses[counter])
+        }
+        
+        
         counter--
         newDiv.appendChild(newButtons)
     }
     buttonBox.appendChild(newDiv)
 }
 
-let firstString = ""
-let secondString = "0"
+
 
 let innerDisplay1 = document.querySelector(".innerDisplay1")
 let innerDisplay2 = document.querySelector(".innerDisplay2")
 
-
+let firstString = innerDisplay1.textContent
+let secondString = innerDisplay2.textContent
 
 function clear(){
     firstString = ""
@@ -80,6 +109,114 @@ function clear(){
     innerDisplay1.textContent = firstString
     innerDisplay2.textContent = secondString
 }
-
-let ce = document.querySelector("#CE")
+function replaceAt(string, index, replacement) {
+    return string.substr(0, index) + replacement + string.substr(index + replacement.length);
+}
+let ce = document.querySelector(".CE")
 ce.addEventListener('click',clear)
+
+let arr = ["+","-","÷","×","="] 
+let arrNumber = [0,1,2,3,4,5,6,7,8,9] 
+let classArray = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+arr.forEach(function (item){
+    let core = ""
+    switch (item){
+        case "+":
+            core = "plus"
+            break
+        case "-":
+            core = "minus"
+            break
+        case "÷":
+            core = "of"
+            break
+        case "×":
+            core = "multiply"
+            break
+        case "=":
+            core = "equals"
+            break
+        case 4:
+            core = "four"
+        default:
+            break
+
+    }
+    core = "." + core;
+    let operators = document.querySelector(core)
+    operators.addEventListener('click',() => {
+    console.log(firstString)
+    
+    console.log(firstString[firstString.length - 2])
+    if (firstString[((firstString.length) - 2)] != "="){
+        if (firstString.length == 0){
+            console.log(firstString) 
+            firstString += "0 " 
+            firstString += operators.textContent
+            firstString += "_"
+        }
+        else if (   ["+","-","÷","×"].includes(firstString[((firstString.length) - 2)])){
+            if(   ["+","-","÷","×"].includes(operators.textContent)) {
+                
+                if (firstString[((firstString.length) - 2)] != operators.textContent) {
+
+                    //firstString = replaceAt(firstString, ((firstString.length) - 2),(" "));
+                    firstString = replaceAt(firstString, ((firstString.length) - 2),(operators.textContent));
+                    
+                        
+                        
+                        
+                    }
+            } 
+            
+        }
+        else if (   ["="].includes(firstString[((firstString.length) - 2)])){
+    
+        }
+        else {
+            firstString += operators.textContent
+            firstString += "_"
+        }
+    }
+    
+    innerDisplay1.textContent = firstString
+})
+})
+
+
+ 
+
+classArray.forEach(function (item){
+
+    let operand = document.querySelector("." + item)
+    operand.addEventListener('click',() => {
+        
+        if(firstString.length == 0){
+            firstString += operand.textContent 
+            firstString += "_"
+            
+        }
+        else if (   ["="].includes(firstString[((firstString.length) - 2)])){
+    
+        }   
+        else{
+            
+            if ((arrNumber.includes(parseInt(operand.textContent))) && (arrNumber.includes(parseInt(firstString[firstString.length - 2] )))){
+                
+                firstString = firstString.substring(0,(firstString.length - 1))
+                console.log(firstString)
+                firstString += operand.textContent
+                firstString += "_"
+            }
+            else{
+                firstString += operand.textContent
+                firstString += "_"
+            }
+            
+            
+        }
+        console.log(firstString)
+        innerDisplay1.textContent = firstString
+        
+    })
+})
